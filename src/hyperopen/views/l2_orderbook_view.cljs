6378342f70c8d627071dfffb4faa5cfbf9a7574c
Geronimo <thegeronimo@protonmail.com>
2025-07-21 15:17:54 -0400

    feat(orderbook): add smooth width transition to orderbook bars for organic animation
    - Bars now animate width changes smoothly using Tailwind transition classes
    - Improves visual fluidity and reduces choppy/teleport effect in orderbook rows

--- src/hyperopen/views/l2_orderbook_view.cljs
+++ src/hyperopen/views/l2_orderbook_view.cljs
@@ -85,7 +85,7 @@
     [:div.flex.items-center.h-8.relative.bg-gray-900
      ;; Size bar background - always positioned from left
      [:div.absolute.inset-0.flex.items-center.justify-start
-      [:div {:class ["h-full" bar-color]
+      [:div {:class ["h-full" bar-color "transition-all" "duration-300" "ease-in-out"]
              :style {:width (str bar-width "%")}}]]
      ;; Content
      [:div.flex.w-full.items-center.justify-between.px-3.relative.z-10
