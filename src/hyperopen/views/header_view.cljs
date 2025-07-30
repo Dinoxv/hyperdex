45c5fb540a331a6adc3fe179dc014134220a2095
Geronimo <thegeronimo@protonmail.com>
2025-07-30 10:01:29 -0400

    fix: correct header brand text to "HyperOpen" with proper styling

--- src/hyperopen/views/header_view.cljs
+++ src/hyperopen/views/header_view.cljs
@@ -6,10 +6,7 @@
     [:div.flex.justify-between.items-center
      ;; Logo and Brand
      [:div.flex.items-center.space-x-3
-      [:div.w-8.h-8.flex.items-center.justify-center
-       [:svg.w-6.h-6.text-white {:viewBox "0 0 24 24" :fill "currentColor"}
-        [:path {:d "M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z"}]]]
-      [:span.text-white.text-xl.font-bold "Hyperliquid"]]
+      [:span.text-primary.text-3xl.font-bold.font-splash "HyperOpen"]]
      
      ;; Navigation Links
      [:nav.hidden.md:flex.items-center.space-x-8
